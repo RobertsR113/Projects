f0743e24325f6b95cfb699b2b491d83f4339fb70
RobertsR113 <Roberts.Rudmanis@gmail.com>
2026-03-11 21:21:37 +0200

    Add files via upload

---
+++ c#-projects/blazor-market-simulator/MarketSimulator/MarketSimulator.SharedObjects/BuyProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSimulator.SharedObjects
+{
+    public class BuyProduct
+    {
+        public Product? Product { get; set; }
+        public int DesiredQuantity { get; set; }
+    }
+}
---
+++ c#-projects/blazor-market-simulator/MarketSimulator/MarketSimulator.SharedObjects/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSimulator.SharedObjects
+{
+    public class Order
+    {
+        public string Name { get; set; }
+        public double TotalItemPrice { get; set; }
+        public int Quantity { get; set; }
+        public string Date { get; set; }
+        public static string formatDate(DateTime date) 
+        {
+            return date.ToString("dd.MM.yyyy HH:mm");
+        }
+        public Order(string date, string name, int requestedAmount, double totalItemPrice)
+        {
+            Date = date;
+            Name = name;
+            TotalItemPrice = totalItemPrice;
+            Quantity = requestedAmount;
+        }
+    }
+}
